706e2f552bbae1a8557a5b1f9b148390cc606458
Spencer15509 <harunaumarsanda4@gmail.com>
2026-01-29 11:13:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ //variables and data types in javascript
+ 
+let firstname ="haruna umar";//string data type
+let z = 15;
+let price = 14.55;// number data type
+const lastname = 'sanda umar';//constant string cannot change
+
+let isdeveloper = true;
+let isbroke = false;//boolean data type
+
+let currency ;//undefined
+let x = 14;//number data type
+let y = null;//empty data type
+
+let student = {
+    regn0 : 1045,
+    age : 17,
+    faculty : computing
+};//object stores collection of key value pairs
+console.log(student.age);
+
+var $colors = [
+     black, red, purple
+];//ordered collection of values in a single variable
+
