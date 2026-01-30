bc2458e33be62e43433771868adc8d1c8fce0868
Spencer15509 <harunaumarsanda4@gmail.com>
2026-01-30 06:18:56 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,56 @@
+//operators and expressions
+
+let sum = 2 + 3;
+let product = 2 * 4;// arithmetic operator
+
+let x = 4;
+let y = x + 5;
+
+let h = 5;
+h += 2 ;//assingmemt operators
+let student = age >= 18;
+ 
+let a = 5 ;
+a -= 4;//subtract and assign
+
+let b = 10;
+b *= 10 ;//multiply and assign
+
+//comparison operators
+let c = 9;
+c == '9';
+c === '9';
+c === 9 ;
+c != 8 ;
+c !== 9;
+c > 10;
+c < 10;
+c <= 10;
+c >= 8;
+
+let j = 10;
+let isequal =  (10 == 9);//false
+let isnotequal = (10 === '10');//true
+
+let notequal = (10 != 9);//true
+let Notequal = (10 !== '10');//true
+let isbigger = (10 > 11);//false
+let issmaller = (10 < 15);//true
+
+//logical operators
+
+let l = 9; 
+let o = 7
+
+//for && if both expression are true then the return is true else false
+(l === 9 && o === 7);//true
+(l !== 9 && o === 7 );//false
+
+//for || if one or both the expressions are true then return is true else false
+(l > 10 || o < 9);//true
+(l < 2 || o > 10);//false
+
+//for (!) is true for false expressions and false for true expressions
+!(l === 9);//false
+!(o < 6);//true
+
