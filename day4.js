41a30dc7846c4e8df289fd00d86932c245654e6f
Spencer15509 <harunaumarsanda4@gmail.com>
2026-02-02 15:02:49 -0800

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,16 @@
+//for loops-repeat a block of code a limited amount of times
+for(let i = 1; i <= 20; i+= 2){
+    console.log(i);
+}
+for(let n = 10; n > 0; n--){
+    console.log(n);
+}
+
+for (h = 0; h <= 8; h++){
+    console.log(h);
+}
+let sum = 0;
+for (f = 0; f <= 9; f++){
+    sum += f;
+}
+console.log(sum);
